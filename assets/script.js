be18402f4ae0003f1d51879616b88dbe193f636e
Maxgazore <167684044+Maxgazore@users.noreply.github.com>
2024-11-08 15:36:27 +0100

    Ajout des flèche du carrousel et des bullet points.

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,13 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener("click", function () {
+	console.log("Fleche gauche")
+})
+arrowRight.addEventListener("click", function () {
+	console.log("Fleche droite")
+})
