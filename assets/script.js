4e993ccd25790c813ff8720de5e762220e1fcb96
Maxgazore <167684044+Maxgazore@users.noreply.github.com>
2024-11-12 18:35:05 +0100

    Refactorisation du code javascript.

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
@@ -24,28 +24,22 @@ let dot = document.querySelectorAll(".dot");
 const arrowRight = document.querySelector(".arrow_right");
 const arrowLeft = document.querySelector(".arrow_left");
 
-arrowRight.addEventListener("click", function () {
+function slideActions(isLeft) {
 	dot[i].classList.remove("dot_selected");
-	if (i == 3) {
-		i = 0;
+	if (isLeft) {
+		i = (i === 0) ? 3 : i - 1;
 	} else {
-		i++;
+		i = (i === 3) ? 0 : i + 1;
 	}
 	bannerImg.src = "./assets/images/slideshow/" + slides[i].image;
 	bannerTagline.innerHTML = slides[i].tagLine;
 	dot[i].classList.add("dot_selected");
-	console.log("Fleche droite");
-})
+}
 
-arrowLeft.addEventListener("click", function () {
-	dot[i].classList.remove("dot_selected");
-	if (i == 0) {
-		i = 3;
-	} else {
-		i--;
-	}
-	bannerImg.src = "./assets/images/slideshow/" + slides[i].image;
-	bannerTagline.innerHTML = slides[i].tagLine;
-	dot[i].classList.add("dot_selected");
-	console.log("Fleche gauche");
-})
+arrowRight.addEventListener("click", () =>
+	slideActions(false)
+)
+
+arrowLeft.addEventListener("click", () =>
+	slideActions(true)
+)
